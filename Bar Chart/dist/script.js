57c68df685d4c872d395775fac2a996b43d5fc60
Arely Miramontes <arelymts@gmail.com>
2019-10-02 18:45:43 -0500

    create x and y axis

--- Bar Chart/dist/script.js
+++ Bar Chart/dist/script.js
@@ -1,27 +1,18 @@
-// !! IMPORTANT README:
+	const w = 700;
+	const h = 500;
+	const padding = 60;
 
-// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+	// Create my SVG
+	const svg = d3.select("#barChart")
+					.append("svg")
+					.attr("width", w)
+					.attr("height", h);
+	
+	// Create X-Axis
+	svg.append("g")
+	.attr("id", "x-axis");
+	
 
-/***********
-INSTRUCTIONS:
-  - Select the project you would 
-    like to complete from the dropdown 
-    menu.
-  - Click the "RUN TESTS" button to
-    run the tests against the blank 
-    pen.
-  - Click the "TESTS" button to see 
-    the individual test cases. 
-    (should all be failing at first)
-  - Start coding! As you fulfill each
-    test case, you will see them go   
-    from red to green.
-  - As you start to build out your 
-    project, when tests are failing, 
-    you should get helpful errors 
-    along the way!
-    ************/
-
-// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
-
-// Once you have read the above messages, you can delete all comments.
+	// Create Y-Axis
+	svg.append("g")
+	.attr("id", "y-axis");
